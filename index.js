9245e72d1e0a0f999e4b89ed9783e41b1375e3de
BaltiBlast <fougeray.florian@gmail.com>
2025-02-19 22:40:47 +0100

    add trigger for nikon with spacebar

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ navigator.mediaDevices
   .getUserMedia({ video: true })
   .then((stream) => (document.getElementById("video").srcObject = stream))
   .catch((error) => console.error("Erreur : ", error));
+
+document.addEventListener("keydown", async (event) => {
+  try {
+    if (event.code === "Space") {
+      const response = await fetch("http://localhost:3000/capture");
+      const data = await response.json();
+
+      if (!data.imageUrl) {
+        throw new Error("L'URL de l'image est invalide !");
+      }
+      photo.src = data.imageUrl + "?t=" + new Date().getTime();
+    }
+  } catch (error) {
+    status.textContent = "Erreur de capture";
+    console.error(error);
+  }
+});
